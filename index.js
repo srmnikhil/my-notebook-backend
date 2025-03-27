468d9b33aed1e4e3304a6f3fb1fd1a316ca56e26
srmnikhil <srmnikhilswn@gmail.com>
2025-03-27 07:57:52 +0530

    added cron job

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/notes", require("./routes/notes"));
+app.use("/", (
+  req, res) => {
+  res.send("Hello World")}
+);
 
 const port = process.env.PORT||5000;
 app.listen(port, () => {
